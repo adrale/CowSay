ef7ed2405630e6c8fa23304a4424904da93560f2
ALEXANDRE MORO <alexandre.moro@live.fr>
2024-03-18 10:15:57 +0100

    CowSay

---
+++ index.js
@@ -0,0 +1,8 @@
+const information = require('./information');
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : `Hi, my name is ${information.name} and I'm from ${information.campus} campus!`,
+    e : "oO",
+    T : "U "
+}));
---
+++ information.js
@@ -0,0 +1,16 @@
+const name = 'Alex';
+const campus = 'Bordeaux';
+// const passions = ['Ski', 'Hiking', 'Cinema'];
+
+// function displayPassions() {
+//   console.log(`My passions are :`);
+//   for (let i = 0; i < passions.length; i += 1) {
+//     console.log(`- ${passions[i]}`);
+//   }
+// }
+
+module.exports = {
+  name: name,
+  campus: campus,
+  // displayPassions: displayPassions,
+};
